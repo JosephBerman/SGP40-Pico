3352529dfbb75586277fcb47876f402a3548ba5a
JosephBerman <berman.jos@northeastern.edu>
2023-03-22 22:12:19 -0400

    testing

---
+++ SGP40.cpp
@@ -0,0 +1,24 @@
+//
+// Created by Waffles on 3/22/23.
+//
+
+#include "SGP40.h"
+
+
+
+void SGP40::getVOC(SGP_Data &data) {
+    uint8_t outBuf[8] = {0x26,0x0F, 0x80, 0x00, 0xA2, 0x66, 0x66, 0x93};
+
+    i2c_write_blocking(i2c_default,_I2CAddress,outBuf, 8, false);
+
+    uint8_t inBuf[3];
+    i2c_read_blocking(i2c_default,_I2CAddress, inBuf, 3, false);
+
+
+    data.voc_raw = (inBuf[0] << 8) + inBuf[1];
+
+    GasIndexAlgorithm_process(&this->params, data.voc_raw, data.voc_index);
+
+}
+
+
---
+++ SGP40.h
@@ -0,0 +1,67 @@
+//
+// Created by Waffles on 3/22/23.
+//
+
+#ifndef CAPSTONESPRING2023_SGP40_H
+#define CAPSTONESPRING2023_SGP40_H
+
+#include <stdio.h>
+#include <math.h>
+#include "gas-index-algorithm/sensirion_gas_index_algorithm/sensirion_gas_index_algorithm.h"
+#include "pico/stdlib.h"
+#include "pico/binary_info.h"
+#include "hardware/i2c.h"
+
+
+typedef struct{
+    int32_t voc_raw;
+    int32_t voc_index;
+} SGP_Data;
+
+
+
+
+class SGP40 {
+private:
+    int _I2CAddress;
+    uint _SDAPin;
+    uint _SCLPin;
+    GasIndexAlgorithmParams params;
+
+
+public:
+    DHT20(uint SDAPin, uint SCLPin, int address = 0x59){
+        _SDAPin = SDAPin;
+        _SCLPin = SCLPin;
+        _I2CAddress = address;
+
+
+
+        gpio_set_function(_SDAPin, GPIO_FUNC_I2C);
+        gpio_set_function(_SCLPin, GPIO_FUNC_I2C);
+        gpio_pull_up(SDAPin);
+        gpio_pull_up(SCLPin);
+        // Make the I2C pins available to picotool
+        bi_decl(bi_2pins_with_func(SDAPin,SCLPin, GPIO_FUNC_I2C));
+        GasIndexAlgorithm_init(&params, GasIndexAlgorithm_ALGORITHM_TYPE_VOC);
+
+
+
+
+
+    }
+
+    void getData(SGP_Data& data);
+
+    bool isReady();
+
+    void intializeRegisters();
+
+
+
+
+
+};
+
+
+#endif //CAPSTONESPRING2023_SGP40_H
